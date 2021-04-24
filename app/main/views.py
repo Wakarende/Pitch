f9a791be60dddcea760afec45ca0eaf4ddc71dfc
Joy Kirii <joykirii@gmail.com>
2021-04-24 19:19:30 +0300

    created pitches model

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+# from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -11,3 +12,23 @@ def index():
   title = 'Pitch'
 
   return render_template('index.html',title = title)
+
+
+@main.route('/pitches/new/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+  form = PitchForm()
+  my_upvotes = Upvote.query.filter_by(pitch_id = Pitch.id)
+  if form.validate_on_submit():
+    description = form.description.data
+    title = form.title.data
+    owner_id = current_user
+    category = form.category.data
+    print(current_user._get_current_object().id)
+    new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
+    db.session.add(new_pitch)
+    db.session.commit()
+        
+        
+    return redirect(url_for('main.index'))
+  return render_template('pitches.html',form=form)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 # from flask_login import UserMixin
 
 class User(db.Model):
@@ -7,7 +8,45 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   username = db.Column(db.String(255))
   email = db.Column(db.String(255),unique = True,index = True)
+  pass_secure = db.Column(db.String(255))
+  pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
   # role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
   def __repr__(self):
     return f'User {self.username}'
 
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  @password.setter
+  def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+
+
+  def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)
+
+class Pitch(db.Model):
+  '''
+  '''
+  __tablename__ = 'pitches'
+
+  id = db.Column(db.Integer, primary_key = True)
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+  description = db.Column(db.String(), index = True)
+  title = db.Column(db.String())
+    # downvotes = db.Column(db.Integer, default=int(0))
+    # upvotes = db.Column(db.Integer, default=int(0))
+  category = db.Column(db.String(255), nullable=False)
+  # comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+  # upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+  # downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+
+    
+  @classmethod
+  def get_pitches(cls, id):
+    pitches = Pitch.query.order_by(pitch_id=id).desc().all()
+    return pitches
+
+  def __repr__(self):
+    return f'Pitch {self.description}'
